2b763a4761f937bc2f36399aa053ca706544adba
Talion426 <106770432+Talion426@users.noreply.github.com>
2022-12-12 04:10:30 +0300

    init (react-budget-app): add app context

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,1 +1,9 @@
 export * from "./types/types";
+
+export * from "./ExpensesContext/ExpensesContext";
+
+export * from "./CurrencyContext/CurrencyContext";
+
+export * from "./BudgetContext/BudgetContext";
+
+export * from "./AppContext/AppContext";
